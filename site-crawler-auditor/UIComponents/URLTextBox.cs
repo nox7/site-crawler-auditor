cebfe2ca51f4593b99d0065d97ac5e16986b2c07
Garet C. Green <17110935+nox7@users.noreply.github.com>
2023-02-24 10:31:48 -0600

    Create URLTextBox.cs

---
+++ site-crawler-auditor/UIComponents/URLTextBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace site_crawler_auditor.UIComponents
+{
+    internal class URLTextBox
+    {
+
+        public TextBox element;
+
+        public URLTextBox(TextBox textBox) {
+            element = textBox;
+            element.GotFocus += onGotFocus;
+        }
+
+        public void onGotFocus(object sender, RoutedEventArgs e)
+        {
+            this.element.Text = "";
+        }
+    }
+}
