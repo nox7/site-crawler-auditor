c04910eb689bc458f25be83eb6019f39d264f1d1
Garet C. Green <17110935+nox7@users.noreply.github.com>
2023-03-09 09:17:53 -0600

    Components for the grids

---
+++ site-crawler-auditor/UIComponents/CrawlGrid/CrawlGrid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace site_crawler_auditor.UIComponents.CrawlGrid
+{
+    internal class CrawlGrid
+    {
+
+        public Grid element;
+
+        public CrawlGrid(Grid element)
+        {
+            this.element = element;
+        }
+    }
+}
---
+++ site-crawler-auditor/UIComponents/CrawlingProgressGrid/CrawlingProgressGrid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace site_crawler_auditor.UIComponents.CrawlingProgressGrid;
+
+internal class CrawlingProgressGrid
+{
+
+    public Grid element;
+
+    public CrawlingProgressGrid(Grid grid)
+    {
+        element = grid;
+    }
+}
