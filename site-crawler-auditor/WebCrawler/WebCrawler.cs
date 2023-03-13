15ea9cb55a52a2efb62055fd23a25a4030f97e31
Garet C. Green <17110935+nox7@users.noreply.github.com>
2023-03-13 09:59:37 -0500

    Wrapper class for crawling a full site

---
+++ site-crawler-auditor/WebCrawler/WebCrawler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace site_crawler_auditor.WebCrawler
+{
+    class WebCrawler
+    {
+
+        private Uri baseUri;
+        private HttpClient httpClient;
+
+        public WebCrawler(Uri baseUri)
+        { 
+            this.baseUri = baseUri;
+            HttpClientHandler httpClientHandler = new HttpClientHandler()
+            {
+                AutomaticDecompression = System.Net.DecompressionMethods.All
+            };
+
+            this.httpClient = new HttpClient(httpClientHandler)
+            {
+                BaseAddress = baseUri
+            };
+        }
+
+        /**
+         * 
+         */
+        public async Task<CrawlResult> CrawlSite()
+        {
+
+        }
+
+        public async Task<CrawledPage> CrawlPage(Uri pageUri)
+        {
+            using HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, pageUri);
+            using HttpResponseMessage response = await this.httpClient.SendAsync(request);
+
+        }
+        
+    }
+}
