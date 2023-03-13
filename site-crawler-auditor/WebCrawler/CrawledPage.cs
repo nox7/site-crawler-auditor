9576a2b58e1fdd68e9ec09e4489f0f0b9837e196
Garet C. Green <17110935+nox7@users.noreply.github.com>
2023-03-13 09:59:21 -0500

    Objects to represent crawl results

---
+++ site-crawler-auditor/WebCrawler/CrawledPage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace site_crawler_auditor.WebCrawler
+{
+    class CrawledPage
+    {
+        private string _url;
+        private List<string> urlsOnPage;
+    }
+}
