f4f73b2ca9b0c23eec69cd5e6cd2b5dded07b288
Garet C. Green <17110935+nox7@users.noreply.github.com>
2023-03-13 09:59:27 -0500

    Create CrawledDataTabControl.cs

---
+++ site-crawler-auditor/UIComponents/CrawledDataTabControl/CrawledDataTabControl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace site_crawler_auditor.UIComponents.CrawledDataTabControl;
+
+internal class CrawledDataTabControl
+{
+
+    public TabControl element;
+
+    public CrawledDataTabControl(TabControl element)
+    {
+        this.element = element;
+    }
+}
